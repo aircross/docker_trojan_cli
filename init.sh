b270c878d2d55c664ae7a46effc69b1a12de742e
aircross <aircross@users.noreply.github.com>
2022-01-01 08:45:13 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,10 +6,10 @@ if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     # 根据参数修改config.json
     # 再执行trojan -c config.json
     # $SERVER $PASSWORD $SP
-    sed -i '/\/trojan_server/s/trojan_server/$SERVER/g' /trojan-cli/config.json
-    sed -i '/\/trojan_pwd/s/trojan_server/$PASSWORD/g' /trojan-cli/config.json
-    sed -i '/\/1080/s/1080/$SP/g' /trojan-cli/config.json
-    /trojan-cli/trojan -c /trojan-cli/config.json
+    # sed -i '/\/trojan_server/s/trojan_server/$SERVER/g' /trojan-cli/config.json
+    # sed -i '/\/trojan_pwd/s/trojan_server/$PASSWORD/g' /trojan-cli/config.json
+    # sed -i '/\/1080/s/1080/$SP/g' /trojan-cli/config.json
+    # /trojan-cli/trojan -c /trojan-cli/config.json
 else
     echo "-- Not first container startup --"
     # 直接执行trojan -c config.json
